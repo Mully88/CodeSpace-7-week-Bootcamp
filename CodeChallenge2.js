417effa0f75d8c005630195b53cb35f4dfbf45e4
Simon Mulligan <95179669+Mully88@users.noreply.github.com>
2026-02-04 13:54:45 +0000

    Update CodeChallenge2.js

--- CodeChallenge2.js
+++ CodeChallenge2.js
@@ -79,37 +79,3 @@ console.log(`My name is ${user.firstNameValue} ${user.lastNameValue}`); */
 
 
 
-//task 3
-
-// Superclass: User
-class User {
-  constructor() {
-    this.username = "";
-  }
-
-  // Setter for username
-  set setUsername(name) {
-    this.username = name;
-  }
-}
-
-// Subclass: Admin inherits User
-class Admin extends User {
-  expressYourRole() {
-    return "Admin";
-  }
-
-  sayHello() {
-    return `Hello admin, ${this.username}`;
-  }
-}
-
-// Create admin object
-const admin = new Admin();
-
-// Set username to "Balthazar"
-admin.setUsername = "Balthazar";
-
-// Output
-console.log(admin.expressYourRole());
-console.log(admin.sayHello());
